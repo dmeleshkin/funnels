cf1b2199d1d782f5e54012234953eb7e320253a9
medial <dmeleshkin@rambler.ru>
2022-03-12 08:01:09 +0300

    bugfix

--- fix.py
+++ fix.py
@@ -29,7 +29,7 @@ events = {
 def isFileInList(fname, flist):
       for f, _ in flist:
             print(f"{f}\n{fname}\n\n")
-            if fname.startwith(f):
+            if fname.startswith(f):
                   return True
       return False
 
